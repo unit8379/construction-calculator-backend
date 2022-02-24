60f7da1f5f807ebb5cc19b0309d7303c137cf18a
unit8379 <glebid76@gmail.com>
2022-02-24 13:42:39 +0400

    Added Group and UserGroup entities

---
+++ src/main/java/com/rpis82/scalc/entity/UserGroup.java
@@ -0,0 +1,28 @@
+package com.rpis82.scalc.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users_groups")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserGroup {
+	
+	@ManyToOne(cascade={CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+	@JoinColumn(name="group_id")
+	private Group group;
+	
+	@OneToOne(cascade={CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+	@JoinColumn(name="user_id")
+	private User user;
+}
