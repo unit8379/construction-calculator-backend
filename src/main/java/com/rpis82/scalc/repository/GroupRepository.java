d10b69f080515bd2bf47cadf35faa47ca80fe0c0
unit8379 <glebid76@gmail.com>
2022-03-05 21:53:11 +0400

    Added GroupRepository (JPA interface)

---
+++ src/main/java/com/rpis82/scalc/repository/GroupRepository.java
@@ -0,0 +1,9 @@
+package com.rpis82.scalc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rpis82.scalc.entity.Group;
+
+public interface GroupRepository extends JpaRepository<Group, Integer> {
+
+}
